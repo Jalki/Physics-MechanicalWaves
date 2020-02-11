a397750c64baab869bf11aa846eba423143b4389
Jalki <jaleenbowenskelly@gmail.com>
2020-02-11 11:36:32 -0500

    2/11 Initial Setup
    This is the initial start of this project!

---
+++ Physics_MechanicalWaves.py
@@ -0,0 +1,65 @@
+#Jaleen Bowens-Kelly
+
+import time
+import math
+
+#speed of a transverse wave
+v
+
+#Speed of sound (average)(m/s)
+vs = 340
+
+#Tension in a string
+F 
+
+#Mass
+mass 
+
+#Length
+length
+
+#Mass per unit length
+mu = mass / length
+
+#y height
+y
+
+#x length
+x
+
+#time
+t
+
+#Amplitude
+A 
+
+#Frequency
+f 
+
+#Harmonic
+n
+
+#lambda
+lam = (length * 2) / n
+
+#Angular frequency
+w = 2 * math.pi * f
+
+#Fundamental Frequency
+fn = n * (v / (2*length))
+
+#Wave function x direction
+xdir = 1
+
+#Wave function of a sinusoidal wave
+y(xt) = A*math.cos(kx-wt)
+
+#Periodic Wave
+v = lam * f
+
+#Period 
+T = 1/f
+
+#Average Power
+Pav = (.5) * math.sqrt(mu*F) * (w**2)*(A**2)
+
